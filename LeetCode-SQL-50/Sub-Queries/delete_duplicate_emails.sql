08b7fa009e22c05925c1bcb6cf16d1d8d91f02bf
Sathish Kumar S <ssathishkumar.118@gmail.com>
2024-06-20 07:03:26 +0530

    delete_duplicate_emails

---
+++ LeetCode-SQL-50/Sub-Queries/delete_duplicate_emails.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+WITH MinID AS (
+    SELECT MIN(id) AS id
+    FROM Person
+    GROUP BY email
+)
+DELETE FROM Person
+WHERE id NOT IN (SELECT id FROM MinID);
